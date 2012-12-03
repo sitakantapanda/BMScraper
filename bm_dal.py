b5a048b85c5579fb31e621e3eda897bfa88a400a
Tejaswi Yerukalapudi <tejaswi.yvs@gmail.com>
2012-12-02 21:54:26 -0500

    bugfixes

--- bm_dal.py
+++ bm_dal.py
@@ -18,26 +18,31 @@ class BMDAL(object):
 	
 	def insert_hobby_info(self, params):
 		query = "INSERT INTO HobbyInfo(hobbies, interests, favorite_music, favorite_cuisine, preferred_dress_style, spoken_languages) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (params["hobbies"], params["interests"], params["favorite_music"], params["favorite_cuisine"], params["preferred_dress_style"], params["spoken_languages"])
-		self.execute(query)
+		self.execute_query(query)
 		return self.last_inserted_row()
 	
 	def insert_location_info(self, params):
 		query = "INSERT INTO LocationInfo(country, state, city, resident_status) VALUES ('%s', '%s', '%s', '%s')" % (params["country"], params["state"], params["city"], params["resident_status"])
-		self.execute(query)
+		self.execute_query(query)
 		return self.last_inserted_row()
 	
 	def insert_professional_info(self, params):
 		query = "INSERT INTO ProfessionalInfo(education, occupation, education_in_detail, occupation_in_detail, employed_in, annual_incom) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (params["education"], params["occupation"], params["education_in_detail"], params["occupation_in_detail"], params["employed_in"], params["annual_incom"])
-		self.execute(query)
+		self.execute_query(query)
 		return self.last_inserted_row()
 	
 	def insert_profile(self, params):
 		query = "INSERT INTO Profile(hobby_info_id, religion_info_id, location_info_id, professional_info_id, family_info_id, partner_preference_id, name, age, height, mother_tongue, eating_habits, smoking_habits, drinking_habits, complexion, physical_status, weight, marital_status) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (params["hobby_info_if"], params["religion_info_id"], params["location_info_id"], params["professional_info_id"], params["family_info_id"], params["partner_preference_id"], params["name"], params["age"], params["height"], params["mother_tongue"], params["eating_habits"], params["drinking_habits"], params["complexion"], params["physical_status"], params["weight"], params["marital_status"])
-		self.execute(query)
+		self.execute_query(query)
 		return self.last_inserted_row()
 	
 	def insert_religion_info(self, params):
-		query = "INSERT INTO(religion, caste, subcaste, gothram, star, kuja_dosham) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (params["religion"], params["caste"], params["subcaste"], params["gothram"], params["star"], params["kuja_dosham"])
+		query = "INSERT INTO ReligionInfo(religion, caste, subcaste, gothram, star, kuja_dosham) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (params["religion"], params["caste"], params["subcaste"], params["gothram"], params["star"], params["kuja_dosham"])
+		self.execute_query(query)
+		return self.last_inserted_row()
+	
+	def insert_partner_preference(self, params):
+		query = "INSERT INTO PartnerPreference(religion_info_id, location_info_id, professional_info_id, groom_age, height, marital_status, mother_tongue, physical_status, eating_habits, smoking_habits, drinking_habits, looking_for) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (params["religion_info_id"], params["location_info_id"], params["professional_info_id"], params["groom_age"], params["height"], params["marital_status"], params["mother_tongue"], params["physical_status"], params["eating_habits"], params["smoking_habits"], params["drinking_habits"], params["looking_for"])
 		self.execute(query)
 		return self.last_inserted_row()
 	
@@ -47,7 +52,8 @@ class BMDAL(object):
 	
 	def last_inserted_row(self):
 		self.cursor.execute("SELECT last_insert_rowid()")
-		return self.cursor.fetchone()
+		text =  self.cursor.fetchone()
+		return text[0]
 
 
 if __name__ == '__main__':
--- profile.py
+++ profile.py
@@ -1,4 +1,5 @@
 import sqlite3
+from bm_dal import BMDAL
 
 class HobbyInfo(object):
 	def __init__(self):
@@ -11,6 +12,17 @@ class HobbyInfo(object):
 		self.preferred_dress_style = ""
 		self.spoken_languages = ""
 	
+	def save(self):
+		dal = BMDAL();
+		params = {}
+		params["hobbies"] = self.hobbies
+		params["interests"] = self.interests
+		params["favorite_cuisine"] = self.favorite_cuisine
+		params["favorite_music"] = self.favorite_music
+		params["preferred_dress_style"] = self.preferred_dress_style
+		params["preferred_movies"] = self.preferred_movies
+		params["spoken_languages"] = self.spoken_languages
+		return dal.insert_hobby_info(params)
 
 
 class ReligionInfo(object):
@@ -23,6 +35,16 @@ class ReligionInfo(object):
 		self.star = ""
 		self.kuja_dosham = ""
 	
+	def save(self):
+		dal = BMDAL();
+		params = {}
+		params["religion"] = self.religion
+		params["caste"] = self.caste
+		params["subcaste"] = self.subcaste
+		params["gothram"] = self.gothram
+		params["star"] = self.star
+		params["kuja_dosham"] = self.kuja_dosham
+		return dal.insert_religion_info(params)
 
 
 class LocationInfo(object):
@@ -33,6 +55,16 @@ class LocationInfo(object):
 		self.city = ""
 		self.resident_status = ""
 		self.citizenship = ""
+	
+	def save(self):
+		dal = BMDAL();
+		params = {}
+		params["country"] = self.country
+		params["state"] = self.state
+		params["city"] = self.city
+		params["resident_status"] = self.resident_status
+		params["citizenship"] = self.citizenship
+		return dal.insert_location_info(params)
 	
 
 
@@ -45,6 +77,17 @@ class ProfessionalInformation(object):
 		self.occupation_in_detail = ""
 		self.employed_in = ""
 		self.annual_income = ""
+	
+	def save(self):
+		dal = BMDAL()
+		params = {}
+		params["education"] = self.education
+		params["occupation"] = self.occupation
+		params["education_in_detail"] = self.education_in_detail
+		params["occupation_in_detail"] = self.occupation_in_detail
+		params["employed_in"] = self["employed_in"]
+		params["annual_income"] = self["annual_income"]
+		return dal.insert_professional_information(params)
 	
 
 class FamilyInfo(object):
@@ -59,6 +102,20 @@ class FamilyInfo(object):
 		self.number_of_brothers = ""
 		self.number_of_sisters = ""
 		self.about_our_family = ""
+	
+	def save(self):
+		dal = BMDAL()
+		params = {}
+		params["family_values"] = self.family_values
+		params["family_type"] = self.family_type
+		params["family_status"] =  self.family_status
+		params["ancestral_origin"] = self.ancestral_origin
+		params["fathers_occupation"] = self.fathers_occupation
+		params["mothers_occupation"] = self.mothers_occupation
+		params["number_of_sisters"] = self.number_of_sisters
+		params["number_of_brothers"] = self.number_of_brothers
+		params["about_our_family"] = self.about_our_family
+		return dal.insert_family_info(params)
 	
 
 class PartnerPreference(object):
@@ -77,6 +134,23 @@ class PartnerPreference(object):
 		self.location_info = LocationInfo()
 		self.professional_info = ProfessionalInformation()
 	
+	def save(self):
+		dal = BMDAL()
+		params = {}
+		params["groom_age"] = self.groom_age
+		params["height"] = self.height
+		params["marital_status"] = self.marital_status
+		params["mother_tongue"] = self.mother_tongue
+		params["physical_status"] = self.physical_status
+		params["eating_habits"] = self.eating_habits
+		params["smoking_habits"] = self.smoking_habits
+		params["drinking_habits"] = self.drinking_habits
+		params["looking_for"] = self.looking_for
+		params["religion_info_id"] = self.religion_info.save()
+		params["location_info_id"] = self.location_info.save()
+		params["professional_info_id"] = self.professional_info.save()
+		return dal.insert_partner_preference(params)
+	
 
 
 class Profile(object):
@@ -93,13 +167,32 @@ class Profile(object):
 		self.weight = ""
 		self.marital_status = ""
 		self.drinking_habits = ""
-		self.religion_info = ReligionInfo()
-		self.location_info = LocationInfo()
-		self.professional_info = ProfessionalInformation()
-		self.family_info = FamilyInfo()
-		self.hobby_info = HobbyInfo()
-		self.partner_preference = PartnerPreference()
+		self.religion_info = None
+		self.location_info = None
+		self.professional_info = None
+		self.family_info = None
+		self.hobby_info = None
+		self.partner_preference = None
 	
 	def save(self):
-		sqlite3.connect('./db/bharat_matrimony.db')
+		dal = BMDAL()
+		params = {}
+		params["religion_info_id"] = self.religion_info.save()
+		params["location_info_id"] = self.location_info.save()
+		params["professional_info_id"] = self.professional_info.save()
+		params["family_info_id"] = self.family_info.save()
+		params["hobby_info_id"] = self.hobby_info.save()
+		params["partner_preference_id"] = self.partner_preference.save()
+		params["name"] = self.name
+		params["age"] = self.age
+		params["height"] = self.height
+		params["mother_tongue"] = self.mother_tongue
+		params["eating_habits"] = self.eating_habits
+		params["smoking_habits"] = self.smoking_habits
+		params["complexion"] = self.complexion
+		params["physical_status"] = self.physical_status
+		params["weight"] = self.weight
+		params["marital_status"] = self.marital_status
+		params["drinking_habits"] = self.drinking_habits
+		return dal.insert_profile(params)
 	
